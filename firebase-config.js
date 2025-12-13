5a64cb66208db4bb27324b4bfcbf824738eedc74
PRATHAM KUMAR <188737331+rajpratham1@users.noreply.github.com>
2025-12-13 14:46:30 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,13 @@
-// This is a placeholder file.
-// On your local machine, and on your Vercel deployment,
-// this file should be replaced with a file containing your
-// actual Firebase configuration.
+// Paste your Firebase project's configuration object here.
+// You can find this in your Firebase project console:
+// Project settings > General > Your apps > Web app > Firebase SDK snippet > Config
 
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY_HERE",
-  authDomain: "PASTE_YOUR_AUTH_DOMAIN_HERE",
-  databaseURL: "PASTE_YOUR_DATABASE_URL_HERE",
-  projectId: "PASTE_YOUR_PROJECT_ID_HERE",
-  storageBucket: "PASTE_YOUR_STORAGE_BUCKET_HERE",
-  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID_HERE",
-  appId: "PASTE_YOUR_APP_ID_HERE"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
