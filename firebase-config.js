394054908525a38367e710565e2c715a445b50a8
PRATHAM KUMAR <188737331+rajpratham1@users.noreply.github.com>
2025-12-13 14:18:34 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// This is a placeholder file.
+// On your local machine, and on your Vercel deployment,
+// this file should be replaced with a file containing your
+// actual Firebase configuration.
+
+const firebaseConfig = {
+  apiKey: "PASTE_YOUR_API_KEY_HERE",
+  authDomain: "PASTE_YOUR_AUTH_DOMAIN_HERE",
+  databaseURL: "PASTE_YOUR_DATABASE_URL_HERE",
+  projectId: "PASTE_YOUR_PROJECT_ID_HERE",
+  storageBucket: "PASTE_YOUR_STORAGE_BUCKET_HERE",
+  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID_HERE",
+  appId: "PASTE_YOUR_APP_ID_HERE"
+};
